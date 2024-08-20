3669be0fb3da09cb8fdf155d355c7f1834a5103d
Yash Rahangdale <rahangdaleyr@rknec.edu>
2024-08-20 18:53:51 +0530

    updated

---
+++ AdvanceSorting.java/MergeTwoSortedArray.java
@@ -0,0 +1,28 @@
+public class MergeTwoSortedArray {
+    public static void print(int []arr){
+        for(int el : arr){
+            System.out.print(el + " ");
+        }
+        System.out.println();
+    }
+    public static void merge(int []a,int []b,int []c){
+        int i=0,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]>=b[j]){
+                c[k++] = b[j++];
+            }
+            else{
+                c[k++] = a[i++];
+            }
+        }
+        while(j<b.length)  c[k++] = b[j++];
+        while(i<a.length)  c[k++] = a[i++];
+    }
+    public static void main(String[] args) {
+        int [] a ={10,50,60,70,90};
+        int [] b = {8,20,40,50,60};
+        int [] c = new int[a.length+b.length];
+        merge(a,b,c);
+        print(c);
+    }
+}
