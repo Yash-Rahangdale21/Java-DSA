51f3378c70b2d4527d82f818779bf3ff59b2d367
Yash Rahangdale <rahangdaleyr@rknec.edu>
2025-05-23 18:41:12 +0530

    add

--- Heaps/KthClosestfromOrigin2.java
+++ Heaps/KthClosestfromOrigin2.java
@@ -1,14 +1,17 @@
 import java.util.*;
-
-public class KthClosestfromOrigin {
-    public  static class Pair{
+public class KthClosestfromOrigin2 {
+    public  static class Triplet implements Comparable<Triplet>{
+        int d;
         int x;
         int y;
-        
-        Pair(int x, int y){
+        Triplet(int d,int x, int y){
+            this.d = d;
             this.x = x;
             this.y = y;
             
+        }
+        public int compareTo(Triplet t){
+            return this.d - t.d;
         }
     }
     // is code ke through kuch case ko solve nhi kar skate to we can use heap without map
@@ -16,15 +19,13 @@ public class KthClosestfromOrigin {
         int[][] points = {{3, 3}, {5, -1}, {-2, 4}, {1, 0},{3,2}};
         int k = 2;
         //MaxHeap
-        PriorityQueue<Integer> MaxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        HashMap<Integer,Pair> map = new HashMap<>();
+        PriorityQueue<Triplet> MaxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
         for(int i =0;i<points.length;i++){
             int x = points[i][0];
             int y = points[i][1];
             int d2 = x*x + y*y;
-            MaxHeap.add(d2);
-            Pair coordinate = new Pair(x, y);
-            map.put(d2,coordinate);
+            MaxHeap.add(new Triplet(d2, x, y));
             if(MaxHeap.size() > k){
                 MaxHeap.remove();
             }
@@ -32,10 +33,10 @@ public class KthClosestfromOrigin {
         int [][] ans = new int[k][2];
         for(int i = 0; i < k; i++){
             //remove the max element from the heap
-            int dist = MaxHeap.remove();
-            Pair coordinate = map.get(dist);
-            ans[i][0] = coordinate.x;
-            ans[i][1] = coordinate.y;
+            Triplet t = MaxHeap.remove();
+            
+            ans[i][0] = t.x;
+            ans[i][1] = t.y;
             
         }
         for(int i = 0; i < k; i++) {
