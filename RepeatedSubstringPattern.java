89ebf23d167faf17bd59edfc855ba573ee735273
Yash Rahangdale <rahangdaleyr@rknec.edu>
2025-08-12 22:08:36 +0530

    add

---
+++ RepeatedSubstringPattern.java
@@ -0,0 +1,13 @@
+public class RepeatedSubstringPattern {
+    public static void main(String[] args) {
+        String s = "abab";
+        System.out.println(repeatedSubstringPattern(s));
+    }
+
+    public static boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String trimmed = doubled.substring(1, doubled.length() - 1);
+        return trimmed.contains(s);
+    }
+
+}
