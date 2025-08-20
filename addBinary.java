12c8c328be5cd18f253ea94afb60d1956797a16b
Yash Rahangdale <rahangdaleyr@rknec.edu>
2025-08-20 22:41:40 +0530

    add

---
+++ addBinary.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class addBinary {
+    public static String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first binary string: ");
+        String a = sc.nextLine();
+        System.out.print("Enter second binary string: ");
+        String b = sc.nextLine();
+        System.out.println("Sum = " + addBinary(a, b));
+        sc.close();
+    }
+}
